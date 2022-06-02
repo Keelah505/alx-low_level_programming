a8db2b83effa9cc2df237266eccd9927b6c56eb0
Aqeelah <aqeelahwingrove@gmail.com>
2022-06-02 07:26:29 -0700

    last tasks

---
+++ 0x00-hello_world/5-ptintf.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+
+/**
+ * printf - prints "with proper grammar, but the outcome is a piece of art"
+ * Return: Always 0 (Success)
+ *     
+ */
+
+void printf(void)
+
+{
+
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+
+			return (0);
+
+}
