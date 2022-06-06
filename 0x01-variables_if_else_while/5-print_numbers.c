46ffb416227042384e90e6261832241b2fa582e0
Aqeelah <aqeelahwingrove@gmail.com>
2022-06-06 03:56:04 -0700

    mandatory

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdlibb>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n";
+		return (0);	
+}
