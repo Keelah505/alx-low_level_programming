e9905b65809f295b0d054c83a868e5ea72dcac71
Aqeelah <aqeelahwingrove@gmail.com>
2022-06-07 06:03:21 -0700

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,13 @@
-r(char c);
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+*File: main.h
+*Header file containing declarations for all  functions used in the 0x02-functions_nested_loops
+*
+*/
+
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -12,3 +21,6 @@ int add(int a, int b);
 void print_to_98(int n);
 void print_times_table(int n);
 void putformat(int);
+
+#endif
+
