dbc15225631cb66aa674f2e813e4603703ed99a1
Aqeelah <aqeelahwingrove@gmail.com>
2022-06-08 06:01:08 -0700

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- *print_alphabet_x10 - print alphabet x10 in lowercase
- *Return: Always 0 (Success)
+ * print_alphabet_x10 - print alphabet x10 in lowercase
  */
 
 void print_alphabet_x10(void)
@@ -13,10 +12,11 @@ void print_alphabet_x10(void)
 	for (j = 0; j < 10; j++)
 	{
 		for (i = 'a'; i <= 'z'; i++)
+		{
 		_putchar(i);
+		}
 	_putchar('\n')
 	}
 
-	return (0);
 }
 
