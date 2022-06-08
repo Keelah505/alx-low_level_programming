7e06d3c6d7bcf1209a02ae23cf0fde5f310e8cdc
Aqeelah <aqeelahwingrove@gmail.com>
2022-06-08 04:36:38 -0700

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,7 +8,14 @@
  */
 int _putchar(char c)
 {
-	write (1, &c, 1);
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
 	
 	return (0);
 }
