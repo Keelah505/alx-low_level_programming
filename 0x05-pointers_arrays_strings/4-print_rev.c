0edb3b5514791b54e592c2ad559eaba3f34d9f06
Aqeelah <aqeelahwingrove@gmail.com>
2022-06-13 02:25:59 -0700

    4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,9 +10,15 @@ void print_rev(char *s)
 {
 	int i;
 
-	for (i = 0; *(s + i); i++)
-		;
-	for (; i > 0; i--)
-		_putchar(*(s + i - 1));
+	for (i = 0; s[i] !='\0'; i++)
+	{
+		continue;
+	}
+	n = i - 1;
+	for (i = n; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
 	_putchar("\n");
+	return;
 }
