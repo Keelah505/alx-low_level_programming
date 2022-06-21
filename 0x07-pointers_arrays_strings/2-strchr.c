af4db752d34bbcaef0cbf8bbfb08dd3326a893dd
Aqeelah <aqeelahwingrove@gmail.com>
2022-06-21 06:18:48 -0700

    1-3

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strchr - fills memory with a constant byte
+ * @s: the memory area to print in
+ * @c: the meory to copy from
+ * Return: r
+ */
+
+char *strchr(char *s, char c)
+{
+	int i;
+	int l;
+
+	while (s[l])
+	{
+		l++;
+	}
+	for (i = 0; i <= l; i++)
+	{
+		if (s[i] == c)
+			return(&(s[i]));
+	}
+	return (0);
+}
