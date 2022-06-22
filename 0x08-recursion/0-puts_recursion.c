f2f7088b0b66440e993bba75f1735d4c01113947
Aqeelah <aqeelahwingrove@gmail.com>
2022-06-22 04:20:38 -0700

    begin

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - fills memory with a constant byte
+ * @s: the memory ara to print in
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	if (s[i] == '\0')
+	{
+		_putchar('\n')
+			return;
+	}
+	_putchar(s[i]);
+	i++;
+	_puts_recursion(s + i);
+}
