8f82ea499a7476616e011ecb43d9a7da9aaa82f8
Aqeelah <aqeelahwingrove@gmail.com>
2022-06-26 02:38:10 -0700

    12

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include stdio.h>
+
+/**
+ * main - entry pont
+ * @argc: number of arguments
+ * @argv: an arrayconsiting arguments
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[argc - 1]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: number of arguemnts
+ * @argv: an array consisting arguments
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	(void)argv;
+	printf("%d\n", argc -1);
+	return (0);
+}
+
