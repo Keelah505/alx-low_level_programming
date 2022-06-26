4d7198c01e8299c33b744062d5a76ad2512a063c
Aqeelah <aqeelahwingrove@gmail.com>
2022-06-26 02:53:57 -0700

    3

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: an array consisting arguemnts
+ * Return: 0
+ */
+
+int main(int argc, int **argv)
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
